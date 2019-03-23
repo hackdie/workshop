7b3e08f0dedff1e8d72813d0de2615bdc762390c
Diego <galmac12@gmail.com>
2019-03-23 17:40:26 +0100

    init

---
+++ android/app/src/main/java/com/workshop/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.workshop
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String? {
+        return "workshop"
+    }
+}
